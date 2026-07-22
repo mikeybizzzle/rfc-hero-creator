fb414da1bbb4a4d2fed28d9b3ac7267363920502
mikeybizzzle <MIKEYATHLETICS@GMAIL.COM>
2026-07-22 16:56:24 -0400

    feat: add two-row horizontal card slider

---
+++ components/card-slider.tsx
@@ -0,0 +1,9 @@
+export function CardSlider({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="overflow-x-auto snap-x snap-mandatory -mx-4 px-4 pb-2">
+      <div className="grid grid-rows-2 grid-flow-col auto-cols-[30vw] sm:auto-cols-[168px] gap-2 sm:gap-3">
+        {children}
+      </div>
+    </div>
+  );
+}
